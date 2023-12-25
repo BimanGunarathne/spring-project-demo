ea45a5866cf97880b3fa8b724c3e2c938bfde287
Biman Gunarathne <bimangunarathne@outlook.com>
2023-12-25 11:20:25 +0530

    update the pom.xml and SpringProjectDemoApplication.java

--- src/main/java/com/spring/springprojectdemo/SpringProjectDemoApplication.java
+++ src/main/java/com/spring/springprojectdemo/SpringProjectDemoApplication.java
@@ -2,9 +2,10 @@ package com.spring.springprojectdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringProjectDemoApplication {
+public class SpringProjectDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectDemoApplication.class, args);
